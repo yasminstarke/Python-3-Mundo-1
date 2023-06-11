65ffbf925c542898ad6e02e70494fdc8730a32b5
yasminstarke <133294823+yasminstarke@users.noreply.github.com>
2023-06-11 02:03:24 -0300

    110623

--- Curso de Pyhton - Exercicios/4 - Condições em Python (if...else)/ex030 - Par ou Impar.py
+++ Curso de Pyhton - Exercicios/4 - Condições em Python (if...else)/ex030 - Par ou Impar.py
@@ -1,1 +1,34 @@
 # Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+
+from time import sleep
+
+n = int(input('Digite um número e te digo se ele é PAR ou ÍMPAR: '))
+par = n % 2 == 0
+if par:
+    sleep(2)
+    print('\nVocê digitou o número {}, ele é PAR.'.format(n))
+else:
+    sleep(2)
+    print('\nVocê digitou o número {}, ele é ÍMPAR.'.format(n))
+
+    ### OUTRA FORMA DE FAZER
+
+'''from time import sleep
+
+n = int(input('Digite um número e te digo se ele é PAR ou ÍMPAR: ')) % 2
+if n == 0:
+    sleep(2)
+    print('\nVocê digitou o número {}, ele é PAR.'.format(n))
+else:
+    sleep(2)
+    print('\nVocê digitou o número {}, ele é ÍMPAR.'.format(n))'''
+
+## OUTRA FORMA
+
+'''número = int(input('Me diga um número qualquer: '))
+resultado = número % 2
+if resultado == 0:
+    print('O número {} é PAR.'.format(número))
+else:
+    print('O número {} é ÍMPAR.'.format(número))'''
+
--- Curso de Pyhton - Exercicios/4 - Condições em Python (if...else)/ex031 - Custo da viagem.py
+++ Curso de Pyhton - Exercicios/4 - Condições em Python (if...else)/ex031 - Custo da viagem.py
@@ -1,2 +1,18 @@
 # Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$0,50 por
 # Km para viagens de até 200km e R$0,45 para viagens mais longas.
+
+distância = float(input('Qual é a distância da sua viagem: ').replace(',','.'))
+print('Você está prestes a começar uma viagem de {}Km.'.format(distância))
+viagem1 = distância * 0.50
+viagem2 = distância * 0.45
+if distância <= 200:
+    print('Sua passagem custará R$ {:.2f}'.format(viagem1))
+else:
+    print('Sua passagem custará R$ {:.2f}'.format(viagem2))
+
+### OUTRA FORMA ####
+
+'''distância = float(input('Qual é a distância da sua viagem? '))
+print('Você está prestes a começar uma viagem de {}Km.'.format(distância))
+preço = distância * 0.50 if distância <= 200 else distância * 0.45
+print('E o preço da sua passagem será de R${:.2f}'.format(preço))'''
